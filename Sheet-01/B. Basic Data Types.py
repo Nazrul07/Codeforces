6eba8c74ec5b3ecf644d51c1419afd82b0d561b9
Nazrul07 <109732672+Nazrul07@users.noreply.github.com>
2024-04-24 00:06:56 +0600

    Create B. Basic Data Types.py

---
+++ Sheet-01/B. Basic Data Types.py
@@ -0,0 +1,9 @@
+# Input values for variables
+a, b, c, f, d = input().split()
+
+# Output the values on separate lines
+print(int(a))
+print(int(b))
+print(c)
+print("{:.2f}".format(float(f)))
+print("{:.5f}".format(float(d)))
