1414b14a9f0ee0f56c73ad726cf612c43852e123
Nazrul07 <109732672+Nazrul07@users.noreply.github.com>
2024-04-24 00:08:08 +0600

    Create C. Simple Calculator.py

---
+++ Sheet-01/C. Simple Calculator.py
@@ -0,0 +1,12 @@
+# Input values for variables
+X, Y = map(int, input().split())
+
+# Perform operations
+summation_result = X + Y
+multiplication_result = X * Y
+subtraction_result = X - Y
+
+# Output the results
+print(f"{X} + {Y} = {summation_result}")
+print(f"{X} * {Y} = {multiplication_result}")
+print(f"{X} - {Y} = {subtraction_result}")
